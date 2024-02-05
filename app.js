eb911833a13ac08d4b004a4381897dee5f2f1246
Muhammad Bilal <mohammadbillal268@gmail.com>
2024-02-05 21:42:31 +0500

    done

--- app.js
+++ app.js
@@ -1,83 +1,66 @@
-
-var minElm = document.getElementsByClassName("min")[0];
-// console.log(minElm)
-var secElm = document.getElementsByClassName("sec")[0];
-// console.log(secElm)
-var milisecElm = document.getElementsByClassName("milli-sec")[0];
-// console.log(milisecElm)
-
-var min = 00 ;
-// minElm.innerHTML = min
-var sec = 00 ;
-// secElm.innerHTML = sec
-var milisec = 00 ;
-// milisecElm.innerHTML = milisec
-
-
-
-var clearIntrval;
-var btnStartStop = document.getElementById("start")
-var boolStrStp = true;
-function handleStartStop(){
-    console.log("test")
-    if(boolStrStp === true){
-        btnStartStop.innerText = "Stop";
-        /****************start wala kam*****************/
-        clearIntrval = setInterval(handleStart,10)
-    }
-    else{
-        btnStartStop.innerText = "Start";
-        /****************stop wala kam*****************/
-        clearInterval(clearIntrval);
-    }
-    boolStrStp = !boolStrStp
+var start = document.getElementById("start");
+var stop = document.getElementById("stop");
+var hours = 0;
+var mins = 0;
+var seconds = 0;
+var miliSeconds = 0;
+var interval;
+var hoursDisp = document.getElementById("hoursDisp");
+console.log(hoursDisp);
+var minsDisp = document.getElementById("minsDisp");
+console.log(minsDisp);
+var secDisp = document.getElementById("secondsDisp");
+console.log(secDisp);
+var miliSecDisp = document.getElementById("miliSecondsDisp");
+console.log(miliSecDisp);
+function display() {
+  hoursDisp.innerHTML = hours;
+  minsDisp.innerHTML = mins;
+  secDisp.innerHTML = seconds;
+  miliSecDisp.innerHTML = miliSeconds;
 }
-btnStartStop.addEventListener("click",handleStartStop)
+display();
 
-
-function handleStart(){
-    milisec++;
-    if(milisec <= 9){
-        milisecElm.innerHTML = "0" + milisec   
+function handleStart() {
+  console.log("Start");
+  start.disabled = true;
+  stop.disabled = false;
+  interval = setInterval(() => {
+    miliSeconds++;
+    miliSecDisp.innerHTML = miliSeconds;
+    if (miliSeconds === 10) {
+      miliSeconds = 0;
+      seconds++;
+      secDisp.innerHTML = seconds;
+      if (seconds == 60) {
+        seconds = 0;
+        mins++;
+        minsDisp.innerHTML = mins;
+        if (mins == 60) {
+          mins = 0;
+          hours++;
+          hoursDisp.innerHTML = hours;
+        }
+      }
     }
-    if(milisec > 9){
-        milisecElm.innerHTML = milisec   
-        
-    }
-    if(milisec >= 99){
-        milisec = 0;
-        // milisecElm.innerHTML = "0" + milisec   
-        sec++;
-        secElm.innerHTML = "0" + sec  + ":" 
-    }
-    if(sec > 9){
-        secElm.innerHTML =  sec   + ":"
-        
-    }
-    if(sec === 10){
-        sec = 0;
-        secElm.innerHTML = "0" + sec
-        min++;
-        minElm.innerHTML = "0" + min + ":"
-    }
-    if(min > 9 ){
-        minElm.innerHTML =  min + ":"
-        
-    }
+  }, 100);
 }
 
-function handleReset(){
-    clearInterval(clearIntrval);
-    min = 0;
-    sec=0;
-    milisec=0;
-    minElm.innerHTML = "00:"
-    secElm.innerHTML = "00:"
-    milisecElm.innerHTML = "00";
-    boolStrStp = true;
-    
-    btnStartStop.innerText = "Start";
-    
+function handleStop() {
+  clearInterval(interval);
+  start.disabled = false;
+  stop.disabled = true;
 }
-var btnReset = document.getElementById("reset");
-btnReset.addEventListener("click",handleReset)
+
+function handleReset() {
+  console.log("Reset");
+  handleStop();
+  hours = 0;
+  mins = 0;
+  seconds = 0;
+  miliSeconds = 0;
+  hoursDisp.innerHTML = 0;
+  minsDisp.innerHTML = 0;
+  secondsDisp.innerHTML = 0;
+  miliSecDisp.innerHTML = 0;
+}
